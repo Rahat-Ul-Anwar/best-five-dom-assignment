8bd8a086df901bb863db2e270f187f3a7846ab76
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-08-23 12:38:11 +0600

    added common function in js file

--- js/index.js
+++ js/index.js
@@ -33,43 +33,49 @@
 const playerButtons = document.querySelectorAll('.card-group button');
 
 const allPlayers = document.getElementById('all-players');
+
+
  
 for (let i = 0; i < playerButtons.length; i++){
     const playerButton = playerButtons[i];
     
     playerButton.addEventListener('click', function () {
-     //showing selected player number
+
+     //getting selected player number
      const selected = document.getElementById('selected');
+                        
+                    // const selectedString = selected.innerText;
+                    // const selectedNumber = parseInt(selectedString);
+                    // const newSelectedNum = selectedNumber + 1;
         
-     const selectedString = selected.innerText;
-     const selectedNumber = parseInt(selectedString);
-     const newSelectedNum = selectedNumber + 1;
+        //callin a function
+      const selectedNum =  selectivePlayer('selected');
         
-        if (newSelectedNum  > 5) {
-    
-          
+        if (selectedNum > 5) {
+        
             alert('can not add more than 5 players');
             return alert;
      }
         else {
            
-            selected.innerText = newSelectedNum;
-            console.log(newSelectedNum);
-
+            selected.innerText = selectedNum;
+            
+    //get the player name from card group
             const playersName = document.querySelectorAll('.card-group h5');
             const playerName = playersName[i];
             const playerNameString = playerName.innerText;
             
-            //create element and set text
+    //create element and set text
          const li = document.createElement('li');
             li.setAttribute("type", "1");
             li.style.color = 'white';
             li.innerText = playerNameString;
             allPlayers.appendChild(li);
-        
-        playerButton.disabled = true;
-        playerButton.style.backgroundColor = 'goldenrod';
-        playerButton.style.color = 'black';
+    
+   //button disable for matching condition
+            playerButton.disabled = true;
+            playerButton.style.backgroundColor = 'goldenrod';
+            playerButton.style.color = 'black';
             
 }
     })
@@ -77,7 +83,42 @@ for (let i = 0; i < playerButtons.length; i++){
 }
 
 
-// selected player and their due
+//*******************************     selected player and their expenses   **********************************
 
+//get the calculate button
+const calculateButton = document.getElementById('calculate-button');
 
+calculateButton.addEventListener('click', function () {
+     
+    const selectedNum =  selectivePlayer('selected');
+    //input field for Budget
+    
+    const inputFieldPlayer = document.getElementById('input-text-player');
+    const inputFieldPlayerString = inputFieldPlayer.value;
+    const inputFieldPlayerAmount = parseInt(inputFieldPlayerString);
  
+    //get the player expenses
+    const playerExpenses = document.getElementById('player-expenses');
+    const playerExpensesString = playerExpenses.innerText;
+    const playerExpensesAmount = parseInt(playerExpensesString);
+    const expenses = inputFieldPlayerAmount * selectedNum;
+
+    console.log(expenses);
+    playerExpenses.innerText = expenses;
+    
+})
+ 
+
+ //common function
+ function selectivePlayer(id) {
+
+    const selected = document.getElementById(id);
+            
+    const selectedString = selected.innerText;
+    const selectedNumber = parseInt(selectedString);
+    const newSelectedNum = selectedNumber + 1;
+        
+    return newSelectedNum;  
+    
+    }
+
