1805d11cbacf605fa650d2aca8587ea69c828b4f
Rahat-Ul-Anwar <anwarrahat69@gmail.com>
2022-08-23 11:58:22 +0600

    added js functionality on selected player

--- js/index.js
+++ js/index.js
@@ -39,42 +39,39 @@ for (let i = 0; i < playerButtons.length; i++){
     
     playerButton.addEventListener('click', function () {
      //showing selected player number
-        const selected = document.getElementById('selected');
+     const selected = document.getElementById('selected');
         
-        const selectedString = selected.innerText;
-        console.log(selectedString);
-        const selectedNumber = parseInt(selectedString);
-        selected.innerText = playerButton.length;
+     const selectedString = selected.innerText;
+     const selectedNumber = parseInt(selectedString);
+     const newSelectedNum = selectedNumber + 1;
         
-        if (playerButton.length > 4) {
+        if (newSelectedNum  > 5) {
     
+          
             alert('can not add more than 5 players');
             return alert;
-}
+     }
         else {
+           
+            selected.innerText = newSelectedNum;
+            console.log(newSelectedNum);
+
             const playersName = document.querySelectorAll('.card-group h5');
             const playerName = playersName[i];
             const playerNameString = playerName.innerText;
-
-
             
-
-           //create element and set text
+            //create element and set text
          const li = document.createElement('li');
             li.setAttribute("type", "1");
             li.style.color = 'white';
             li.innerText = playerNameString;
-        allPlayers.appendChild(li);
+            allPlayers.appendChild(li);
         
         playerButton.disabled = true;
         playerButton.style.backgroundColor = 'goldenrod';
         playerButton.style.color = 'black';
             
-
-            
-         
-            
-        }
+}
     })
     
 }
